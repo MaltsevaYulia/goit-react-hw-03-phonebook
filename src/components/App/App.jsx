67739f6d183bfca90d75f077d3585cf7919118bf
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-03-30 10:29:37 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,16 +5,34 @@ import { Filter } from '../Filter/Filter';
 import { nanoid } from 'nanoid';
 import css from './App.module.css';
 
+const LS_CONTACTS='contacts'
+
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(LS_CONTACTS));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevContacts = prevState.contact;
+    const newContacts = this.state.contacts;
+
+    if (prevContacts !== newContacts) {
+      localStorage.setItem(LS_CONTACTS, JSON.stringify(newContacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     this.setState(prev => {
@@ -50,7 +68,7 @@ export class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
     return (
-      <div className={(css.container)}>
+      <div className={css.container}>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts</h2>
